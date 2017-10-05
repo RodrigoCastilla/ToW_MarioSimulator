1a5695894090cd05adf13add5a537b74c0e39367
Rodrigo <armc3po@gmail.com>
2017-10-05 02:46:48 -0500

    Initializing

---
+++ Assets/Script/GUI/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	private string mainSceneName;
+
+	// Use this for initialization
+	void Start () {
+		mainSceneName = "GameScene";
+	}
+
+	public void ChangeToPlayScene(){
+		SceneManager.LoadScene (mainSceneName);
+	}
+}
---
+++ Assets/Script/Mario/MarioMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioMovement : MonoBehaviour {
+	public MarioSoundFX soundEffects;
+	private float velocityOnX, jumpVelocity, movementOnX, actualPosition;
+	private Animator movementAnimator;
+
+	// Use this for initialization
+	void Start () {
+		movementAnimator = GetComponent<Animator> ();
+		velocityOnX = 0.1f; 
+		jumpVelocity = 500f;
+		movementOnX = actualPosition = 0; 
+	}
+		
+	private void MoveMario(float horizontalMovement){
+		movementAnimator.SetBool ("movementOnX", true);
+		movementOnX = transform.position.x + (horizontalMovement * velocityOnX);
+		transform.position = new Vector3 (movementOnX, transform.position.y, 2);
+		if (horizontalMovement > 0) {
+			transform.localScale = new Vector3 (4,4,3);
+		} else if (horizontalMovement < 0){
+			transform.localScale = new Vector3 (-4,4,3);
+		}
+		movementOnX = actualPosition;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			soundEffects.PlayMarioJumpFX ();
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, jumpVelocity));
+		} 
+		float inputX = Input.GetAxis ("Horizontal");
+		if (inputX != 0)
+			MoveMario (inputX);
+		else 
+			movementAnimator.SetBool ("movementOnX", false);
+
+
+	}
+}
